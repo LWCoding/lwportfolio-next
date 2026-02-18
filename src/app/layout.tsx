bda9938293e75655d3306e23f96e658cf49ba4d5
LWCoding <DiamondMob01@gmail.com>
2026-02-18 03:40:42 -0800

    Imported Vercel analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import { Analytics } from "@vercel/analytics/react";
 import "./globals.css";
 import ScrollToTopButton from "@/components/ScrollToTopButton";
 
@@ -44,6 +45,7 @@ export default function RootLayout({
       >
         {children}
         <ScrollToTopButton />
+        <Analytics />
       </body>
     </html>
   );
